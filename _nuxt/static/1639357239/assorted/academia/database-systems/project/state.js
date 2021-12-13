6c2f585299925c254d4cd6f0b997aaa25660f173
Marcelo Fernandes <marceelofernandes@gmail.com>
2021-12-13 21:09:17 +1300

    site update

--- _nuxt/static/1639357239/assorted/academia/database-systems/project/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639357239",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Facademia\u002Fdatabase-systems\u002Fproject",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1639382933/assorted/chess/basic-checkmates/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/assorted/chess/basic-checkmates", {data:[{}],fetch:{},mutations:void 0});
