f800fbd4f92113e1a812b6df0a4f0fb76a01db0a
Marcelo Fernandes <marceelofernandes@gmail.com>
2021-12-26 18:02:24 +1300

    site update

--- _nuxt/static/1639883791/assorted/books/beyond-good-and-evil-nietzsche/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639883791",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Fbooks\u002Fbeyond-good-and-evil-nietzsche",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1640494926/assorted/gallery/2021-12-25-new-plymouth/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/assorted/gallery/2021-12-25-new-plymouth", {data:[{}],fetch:{},mutations:void 0});
