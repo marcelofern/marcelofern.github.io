1b0470f898f1f92afc8a59837dffc78ec60d8827
Marcelo Fernandes <marceelofernandes@gmail.com>
2022-01-29 10:52:51 +1300

    site update

--- _nuxt/static/1642749407/assorted/chess/protecting-the-king/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642749407",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Fchess\u002Fprotecting-the-king",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1642749407/assorted/gallery/2022-01-02-ben-lomond/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642749407",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Fgallery\u002F2022-01-02-ben-lomond",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
