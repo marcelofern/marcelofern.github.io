5ec288524ced9c8e77584db6933a45fe1b5f388a
Marcelo Fernandes <marceelofernandes@gmail.com>
2022-01-29 12:50:07 +1300

    site update

---
+++ _nuxt/static/1643413789/assorted/academia/mathematical-logic/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643413789",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Facademia\u002Fmathematical-logic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
