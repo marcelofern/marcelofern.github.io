87a373c8d872eafefde20d8b94278ae5d4c1203a
Marcelo Fernandes <marceelofernandes@gmail.com>
2022-02-12 21:56:07 +1300

    site update

--- _nuxt/static/1643413789/assorted/gallery/2022-01-07-queenstown/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643413789",layout:"default",error:a,serverRendered:true,routePath:"\u002Fassorted\u002Fgallery\u002F2022-01-07-queenstown",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
